1122b9bdf4fa28f250708509e485eb07fb207ad1
E1jeong <won994@gmail.com>
2022-06-01 10:32:19 +0900

    js강의 - sound module

--- carrot/src/field.js
+++ carrot/src/field.js
@@ -1,7 +1,7 @@
 "use strict";
+import * as sound from "./sound.js";
 
 const CARROT_SIZE = 80;
-const carrotSound = new Audio("./sound/carrot_pull.mp3");
 
 export default class Field {
   constructor(carrotCount, bugCount) {
@@ -46,17 +46,12 @@ export default class Field {
     const target = event.target;
     if (target.matches(".carrot")) {
       target.remove();
-      playSound(carrotSound);
+      sound.playCarrot();
       this.onItemClick && this.onItemClick("carrot");
     } else if (target.matches(".bug")) {
       this.onItemClick && this.onItemClick("bug");
     }
   }
-}
-
-function playSound(sound) {
-  sound.currentTime = 0;
-  sound.play();
 }
 
 function randomNumber(min, max) {
